4210f451cd4092c8c052c87dda5313b26b2b7a42
Cory Simmons <cory@mojotech.com>
2014-03-07 18:39:38 -0500

    [cleanup] Project structure

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['Gruntfile.js', 'jeet.js', 'lib/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('dev', ['jshint', 'watch']);
+
+};
--- jeet.js
+++ jeet.js
@@ -1,27 +1,3 @@
-var postcss = require('postcss'),
-    fs = require('fs'),
-    inFile = fs.readFileSync('./in.css'),
-    css = postcss.parse(inFile);
+#!/usr/bin/env node
 
-var contenter = postcss(function (css) {
-    css.eachRule(function (rule) {
-        if ( rule.selector.match(/.foo/) ) {
-
-            rule.eachDecl(function (decl, i) {
-              if(decl.prop == 'span') {
-                var frac = decl._value,
-                    ratioW = (frac.split("/")[0] / frac.split("/")[1] * 100);
-                rule.remove(i);
-                rule.prepend({prop: 'float', value: 'left'});
-                rule.prepend({prop: 'width', value: ratioW + '%'});
-              }
-            });
-
-
-        }
-    });
-});
-
-var fixed = contenter.process(css).css;
-
-fs.writeFileSync('out.css', fixed);
+require("./lib/index.js");
